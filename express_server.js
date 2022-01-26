3ae327bda261bfe24111d99a9844c71e961fb0ff
willianchu <willianchu@hotmail.com>
2022-01-25 20:48:02 -0800

    finished the last function update

--- express_server.js
+++ express_server.js
@@ -11,22 +11,24 @@ app.set("view engine", "ejs"); // set ejs as the view engine - must be installed
 
 const urlDatabase = disk2object("urlDatabase.json"); // retrieve the object from disk  and convert it to a javascript object
 
-app.get("/urls/new", (req, res) => { // GET form doesn't have a body
-  res.render("urls_new");
-});
-
-
 // ##### Browse - Beginning of the URL #####
 // #########################################
 app.get("/urls", (req, res) => {
+  console.log("### load index page ###");
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 // ##### Write/ Create #####
 // #########################
-app.post("/urls", (req, res) => {
+app.get("/urls/new", (req, res) => { // GET form doesn't have a body
+  console.log("### load urls_new ###");
+  res.render("urls_new");
+}); // >>>>>>>>>> show
+
+app.post("/urls", (req, res) => { // <<<<<<<<<<<<<<<< write
   const uniqueKey = generateRandomString(6, urlDatabase);
+  console.log(">>> post /urls adding a new tiny <<<");
   console.log(uniqueKey, req.body);  // Log the POST request body to the console
   urlDatabase[uniqueKey] = req.body.longURL; // add the new URL to the database
   object2disk(urlDatabase, "urlDatabase.json"); // save the database to disk
@@ -35,7 +37,8 @@ app.post("/urls", (req, res) => {
 
 // ##### Erase/ Delete #####
 // #########################
-app.post("/urls/*/delete", (req, res) => {
+app.post("/urls/*/delete", (req, res) => { //<<<<<<<<<<<<<<<< delete
+  console.log(">>>> post Delete <<<<<")
   console.log(req.body);  // Log the POST request body to the console
   delete urlDatabase[req.params[0]]; // delete the URL from the database
   object2disk(urlDatabase, "urlDatabase.json"); // save the database to disk
@@ -46,33 +49,37 @@ app.post("/urls/*/delete", (req, res) => {
 // ##### Go to the long URL #####
 // ##############################
 app.get("/u/:shortURL", (req, res) => {
-  // const longURL = ...
+  console.log("Go to the long URL >>>>>>>> external");
   console.log(req.params);
   const longURL = urlDatabase[req.params.shortURL]; // requesting the longURL from the database using the shortURL in the params of the request
   
   console.log(longURL);
-  res.redirect(longURL); // go outside 
+  res.redirect(longURL); // go outside
 });
 
 
 // ##### specific URL #####
 // #########################
 app.get("/urls/:shortURL", (req, res) => {
+  console.log("### Shows Specific URL ####");
   console.log(urlDatabase[req.params.shortURL]);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_edit", templateVars);
+  res.render("urls_show", templateVars);
 });
 
 // ##### Update/ Edit #####
 // #########################
-app.get("/urls/edit/:idx", (req, res) => {
-  const index = req.params.idx;
-  const templateVars = { shortURL: index, longURL: urlDatabase[index] };
+app.get("/urls/edit/:shortURL", (req, res) => {
+  const index = req.params.shortURL;
+  console.log("### Shows EDIT URL ####");
+  console.log(index, urlDatabase[req.params.shortURL]);
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_edit", templateVars);
 });
 
 app.post("/urls/edit/:shortURL", (req, res) => {
-  console.log(urlDatabase[req.params.shortURL]);
+  console.log("### Update req ####");
+  console.log(req.params.shortURL, req.body, req.body.shortURL);
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
@@ -89,15 +96,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-// app.get("/hello", (req, res) => {
-//   const templateVars = { greeting: 'Hello World!' };
-//   res.render("hello_world", templateVars);
-// });
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-// app.get("/urls.json", (req, res) => { // json path
-//   res.json(urlDatabase);
-// });
