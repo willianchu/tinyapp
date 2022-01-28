86325b3c23ed493ff68b3ac10ff9fdd6c70ab2f4
willianchu <willianchu@hotmail.com>
2022-01-27 18:33:27 -0800

    images for readme.md

--- express_server.js
+++ express_server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
-const generateRandomString = require("./random"); // @willianchu random.js
-const { object2disk, disk2object } = require("./object2disk"); // @willianchu object2disk.js
 const bcrypt = require('bcryptjs');
+const generateRandomString = require("./random"); // generateRandomString
+const { object2disk, disk2object } = require("./object2disk"); // write to disk
+const PORT = 8080; // default port 8080
+
 
 // middleware
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,148 +22,129 @@ app.use(cookieSession({
 // ##### URL data base disk retrieve #####
 const urlDatabase = disk2object("urlDatabase.json"); // retrieve the object from disk  and convert it to a javascript object
 
+/*  email":"elonmusk@tesla.com","password": "elon123"
+    email":"billgates@microsoft.com", "password": "bill123"
+    "email":"jeff@amazon.com"","password": "besos123"
+    "email":"alicia@gmail.com","password": "silverstone"
+    "email":"billyidol@gmail.com","password": "rockstar"
+*/
+    
 // ##### users database disk retrieve #####
 const usersDatabase = disk2object("usersDatabase.json"); // retrieve the object from disk  and convert it to a javascript object
 
 
-// ##### Login/ Cookie route #####
-app.get("/login", (req, res) => {
-  console.log("render login page"); // >>>>>>>>>>>>> GET login page
+// ##### /login route - login page #####
+app.get("/login", (req, res) => { // GET login
   const validId = req.session.userID ? req.session.userID : false;
   const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   const templateVars = { "user_id": validId, "user_email": validEmail };
-  console.log("templateVars delivered", templateVars);
   res.render("login", templateVars);
 });
 
 
-app.post("/login", (req, res) => { // <<<<<<<<<<<<<<< POST login
-  console.log("Entered in POST /login (req.body)=", req.body);
-  console.log("POST req.body.password", req.body.password);
-  const loggedUserEmail = req.body.email;
-  const loggedUserPassword = req.body.password; // get the password from the form
-  console.log("loggedUserEmail >>>", loggedUserEmail, "loggedUserPassword", loggedUserPassword);
-  let encryptIsTrue = false;
-  for (let user in usersDatabase) { // loop through the users database
-    encryptIsTrue = bcrypt.compareSync(loggedUserPassword, usersDatabase[user].password); // compare the password from the form with the password from the database
-    console.log("encryptIsTrue", encryptIsTrue);
-    console.log("usersDatabase[user].email", usersDatabase[user].email);
-    if (usersDatabase[user].email === loggedUserEmail && encryptIsTrue) { // if the email and password match
-      req.session.userID = usersDatabase[user].id; // set cookie session
-      res.redirect("/urls"); // redirect to the urls page
-      res.end();
-    }
+app.post("/login", (req, res) => { // POST /login
+  const formUserEmail = req.body.email;
+  const formUserPassword = req.body.password;
+  const validUser = checkCredentials(formUserEmail, formUserPassword);
+  if (validUser) {
+    console.log("usersDatabase", usersDatabase[validUser].id);
+    req.session.userID = usersDatabase[validUser].id;
+    res.redirect("/urls");
+    res.end();
+  } else {
+    res.status(403).send("<h1>Invalid email or password</h1>");
+    res.end();
   }
-  res.status(403).send("<h1>email/ password don't mach!</h1>"); // if the email and password don't match
-  res.end();
 });
 
 
-// ##### Logout #####
-app.post("/logout", (req, res) => { // <<<<<<<<<<<<<<< POST logout erase cook
-  console.log("### Logout Post ####");
+// ##### /logout - button #####
+app.post("/logout", (req, res) => { // POST logout erase cookie
   req.session = null;
   res.redirect("/urls");
 });
 
 
 
-// ##### Browse - Beginning of the URL - main page #####
+// ##### /urls - main page #####
 app.get("/urls", (req, res) => {
-  console.log("### load index page ###");
   const validId = req.session.userID ? req.session.userID : false;
-  //look for a valid email using the cookie
   const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   if (validId === false) { // if the user is not logged in
     res.redirect("/first");
     res.end();
   }
-  // sends only the urls that the user owns
   const userUrls = {};
-  for (let url in urlDatabase) {
-    console.log(urlDatabase[url].userID,validId);
+  for (let url in urlDatabase) {// sends only the urls that the user owns
     if (urlDatabase[url].userID === validId) {
       userUrls[url] = urlDatabase[url];
     }
   }
-  console.log("userUrls", userUrls);
   const templateVars = { "user_id": validId, "user_email": validEmail, "urls": userUrls };
   res.render("urls_index", templateVars);
 });
 
 
-// ##### Register #####
-app.get("/register", (req, res) => { //>>>>>>>> GET register page
-  console.log("### register ###");
+// ##### /register route - registration page #####
+app.get("/register", (req, res) => { // GET register page
   const validId = req.session.userID ? req.session.userID : false;
   const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   const templateVars = { "user_id": validId, "user_email": validEmail };
   res.render("register", templateVars);
 });
 
 
-app.post("/register", (req, res) => { // <<<<<<<<<<<< POST form has a body
+app.post("/register", (req, res) => { // POST form from register page
   const newEmail = req.body.email;
   const newPassword = req.body.password;
-  if (newEmail === "" || newPassword === "") {
+  if (newEmail === "" || newPassword === "") { 
     res.status(400).send("Please enter a valid email and password");
-  }
-
-  // check if email is already in use in users database
+  } // if the email or password is empty
   for (let user in usersDatabase) {
     if (usersDatabase[user].email === newEmail) {
       res.status(400).send("Email already in use");
-      return;
+      return; // if the email is already in use
     }
   }
   const newUserId = generateRandomString(6);
-  console.log("Register valid");
-  console.log(newUserId, newEmail, newPassword);
   const encryptedPassword = bcrypt.hashSync(newPassword, 10);
-  usersDatabase[newUserId] = { "id": newUserId, "email": newEmail, "password": encryptedPassword };
-  object2disk(usersDatabase, "usersDatabase.json");
-  console.log("user",usersDatabase);
+  usersDatabase[newUserId] = {
+    "id": newUserId,
+    "email": newEmail,
+    "password": encryptedPassword
+  };
+  object2disk(usersDatabase, "usersDatabase.json"); // write to disk
   req.session.userID = newUserId; // set cookie session
   res.redirect("/urls"); // redirect to the urls page
   res.end();
 });
 
 
-// ##### Write/ Create #####
-app.get("/urls/new", (req, res) => { // >>>>>>>>>> GET page
-  console.log("### load urls_new ###");
+// ##### /urls/new - Creates a new tiny link #####
+app.get("/urls/new", (req, res) => { // GET new link page
   const validId = req.session.userID ? req.session.userID : false;
   const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   const templateVars = { "user_id": validId, "user_email": validEmail };
   res.render("urls_new", templateVars);
-}); // >>>>>>>>>> show
+});
 
-app.post("/urls", (req, res) => { // <<<<<<<<<<<<<<<< POST form
-  const uniqueKey = generateRandomString(6);
+app.post("/urls", (req, res) => { // POST form with new URL link
+  const newUserKey = generateRandomString(6); // generate a user key
   const validId = req.session.userID ? req.session.userID : false;
-  const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
-  console.log(">>> post /urls adding a new tiny <<<");
-  console.log(uniqueKey, req.body);  // Log the POST request body to the console
-  urlDatabase[uniqueKey] = { longURL: req.body.longURL, userID: validId}; // add the new URL to the database
+  urlDatabase[newUserKey] = {
+    longURL: req.body.longURL,
+    userID: validId
+  }; // add the new URL to the database
   object2disk(urlDatabase, "urlDatabase.json"); // save the database to disk
   res.redirect("/urls");  // Redirect to the index page
 });
 
 
-// ##### Erase/ Delete #####
-app.post("/urls/*/delete", (req, res) => { // post <<<<<<<<<<<<<<<< delete
-  console.log(">>>> post Delete <<<<<");
-  console.log(req.body);  // Log the POST request body to the console
+// ##### Erase/ Delete tiny urls #####
+app.post("/urls/*/delete", (req, res) => { // post button delete url
   const validId = req.session.userID ? req.session.userID : false;
-  const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   if (validId === urlDatabase[req.params[0]].userID) {
-    delete urlDatabase[req.params[0]]; // delete the URL from the database
+    delete urlDatabase[req.params[0]]; // delete user's url by user
     object2disk(urlDatabase, "urlDatabase.json"); // save the database to disk
     res.redirect("/urls");
   } else {
@@ -171,94 +153,101 @@ app.post("/urls/*/delete", (req, res) => { // post <<<<<<<<<<<<<<<< delete
 });
 
 
-// ##### Go to the long URL #####
+// ##### Go to the long URL Call #####
 app.get("/u/:shortURL", (req, res) => {
-  console.log("Go to the long URL >>>>>>>> external");
-  console.log(req.params);
-  const longURL = urlDatabase[req.params.shortURL].longURL; // requesting the longURL from the database using the shortURL in the params of the request
-  // check if the longURL is a valid URL
-  if (longURL) {
-    console.log(longURL);
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  if (longURL) { // check if the longURL is a valid URL
     res.redirect(longURL); // go outside
+    res.end();
   } else {
     res.redirect("/notfound");
+    res.end();
   }
 });
 
 
-// ##### specific URL #####
+// ##### search for specific URL #####
 app.get("/urls/:shortURL", (req, res) => {
-  console.log("### Shows Specific URL ####");
-  console.log("parameters",req.params.shortURL);
   if (urlDatabase[req.params.shortURL]) {
     const validId = req.session.userID ? req.session.userID : false;
     const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-    console.log("validId", validId); // *** update
-    const templateVars = { "user_id": validId, "user_email": validEmail, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL };
+    const templateVars = {
+      "user_id": validId,
+      "user_email": validEmail,
+      "shortURL": req.params.shortURL,
+      "longURL": urlDatabase[req.params.shortURL].longURL
+    };
     res.render("urls_show", templateVars);
+    res.end();
   } else {
     res.redirect("/notfound");
+    res.end();
   }
 });
 
 
-// ##### Update/ Edit #####
-app.get("/urls/edit/:shortURL", (req, res) => { // GET >>>>>>>>>>>>>>>>>
-  const index = req.params.shortURL;
-  console.log("### Shows EDIT URL ####");
-  console.log(index, urlDatabase[req.params.shortURL]);
+// ##### /urls/edit route - Update/ Edit links #####
+app.get("/urls/edit/:shortURL", (req, res) => { // GET page to edit the link
   const validId = req.session.userID ? req.session.userID : false;
   const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   if (validId === urlDatabase[req.params.shortURL].userID) {
-    const templateVars = { "user_id": validId, "user_email": validEmail, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL};
+    const templateVars = { // send the user's info requested by the own
+      "user_id": validId,
+      "user_email": validEmail,
+      "shortURL": req.params.shortURL,
+      "longURL": urlDatabase[req.params.shortURL].longURL};
     res.render("urls_edit", templateVars);
+    res.end();
   } else {
-    res.redirect("/urls");
-  };
+    res.redirect("/urls"); // if the user is not the owner of the link
+    res.end();
+  }
 });
 
-app.post("/urls/edit/:shortURL", (req, res) => { // POST <<<<<<<<<<<<<<<< Edit
-  console.log("### Edit Post ####");
-  console.log(req.params.shortURL, req.body, req.body.shortURL);
+app.post("/urls/edit/:shortURL", (req, res) => { // POST Edit form
   const validId = req.session.userID ? req.session.userID : false;
-  const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
-  if (validId === urlDatabase[req.params.shortURL].userID) {
+  if (validId === urlDatabase[req.params.shortURL].userID) { //Update the link
     urlDatabase[req.params.shortURL].longURL = req.body.longURL;
     res.redirect("/urls");
     res.end();
-  } else {
+  } else { // if the user is not the owner of the link
     res.status(403).send("<h1>You are not authorized to edit this URL</h1>");
   }
 });
 
 
-// ##### page log in First #####
+// ##### Page log in First #####
 app.get('/first', (req, res) => {
-  console.log("### log in first page ####");
-  // status denied
-  res.status(401);
   const validId = req.session.userID ? req.session.userID : false;
   const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   const templateVars = { "user_id": validId, "user_email": validEmail };
-  res.render("first", templateVars);
+  res.status(401).render("first", templateVars);
   res.end();
 });
 
 // ##### page 404 #####
 app.get('*', (req, res) => {
-  console.log("### 404 page ####");
-  res.status(404);
   const validId = req.session.userID ? req.session.userID : false;
   const validEmail = req.session.userID ? usersDatabase[validId].email : false;
-  console.log("validId", validId); // *** update
   const templateVars = { "user_id": validId, "user_email": validEmail };
-  res.render("urls_404", templateVars);
+  res.status(404).render("urls_404", templateVars);
   res.end();
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+// ##### Functions Helper #####
+const checkCredentials = (email, password) => {
+  for (let user in usersDatabase) {
+    if (usersDatabase[user].email === email) {
+      if (bcrypt.compareSync(password, usersDatabase[user].password)) {
+        console.log("user found",user);
+        return user;
+      }
+    }
+  }
+  return false;
+};
